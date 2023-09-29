aa5f04acf97972c5ec4912a2cb7445a613bbbcd4
Qkdrmf <djs110615@mju.ac.kr>
2023-09-29 21:17:02 +0900

    Feat: 의사 병원 카테고리

---
+++ src/components/DoctorReviewDto.tsx
@@ -0,0 +1,12 @@
+export type DoctorReviewDto = {
+    doctorName: string;
+    hospitalName: string;
+    id: number;
+    location: string;
+    partList: string[];
+    profile: string;
+    recentCreatedAt: string;
+    totalCommentCount: number;
+    totalElements: number;
+    totalViewCount: number;
+};
---
+++ src/review_page/DocsHosReviewItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DoctorReviewDto } from "../components/DoctorReviewDto";
+
+const DocsHosReviewItem = (props: DoctorReviewDto) => {
+    return (
+        <div>
+            <p>DocsHosReviewItem</p>
+        </div>
+    );
+}
+
+export default DocsHosReviewItem;
