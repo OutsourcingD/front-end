1a39f826ac2c54902c0ff9fa5446a7e8de8134e1
Qkdrmf <djs110615@mju.ac.kr>
2023-10-06 04:04:47 +0900

    Feat: 문의 내역 관리

--- src/components/InqueryManagement.tsx
+++ src/components/InqueryManagement.tsx
@@ -1,15 +1,82 @@
 import React from "react";
 import "./InqueryManagement.css";
 import Pagination from "react-js-pagination";
+import axios from "axios";
+
+interface InqueryManagementProps {
+    id: number;
+    userId: string;
+    content: string;
+    answer: string;
+    createdAt: string;
+}
 
 const InqueryManagement = () => {
-    const item = [1, 2, 3, 4, 5, 6, 7, 8];
     const [page, setPage] = React.useState(1);
     const [totalPages, setTotalPages] = React.useState(2);
+    const [id, setId] = React.useState("");
+    const [inquiryList, setInquiryList] = React.useState<InqueryManagementProps[]>([]);
 
     const handlePageChange = (page: React.SetStateAction<number>) => {
         setPage(page);
     }
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        axios({
+            method: "get",
+            url: `${process.env.REACT_APP_SERVER_URL}/admin/inquiry`,
+            params: {
+                pages: page - 1,
+                userId: id,
+            },
+            headers: {
+                Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
+            },
+        }).then((res) => {
+            console.log("click")
+            setInquiryList(res.data);
+            setTotalPages(res.data.length === 0 ? 1 : res.data[0].totalPages);
+            setPage(1);
+        });
+    }
+
+    const onClick = () => {
+        axios({
+            method: "get",
+            url: `${process.env.REACT_APP_SERVER_URL}/admin/inquiry`,
+            params: {
+                pages: page - 1,
+                userId: id,
+            },
+            headers: {
+                Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
+            },
+        }).then((res) => {
+            console.log("click")
+            setInquiryList(res.data);
+            setTotalPages(res.data.length === 0 ? 1 : res.data[0].totalPages);
+            setPage(1);
+        });
+    }
+
+    React.useEffect(() => {
+        axios({
+            method: "get",
+            url: `${process.env.REACT_APP_SERVER_URL}/admin/inquiry`,
+            params: {
+                pages: page - 1,
+                userId: id,
+            },
+            headers: {
+                Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
+            },
+        }).then((res) => {
+            setInquiryList(res.data);
+            setTotalPages(res.data.length === 0 ? 1 : res.data[0].totalPages);
+        });
+    }, []);
 
     return (
         <div className="check_user_ip_page">
@@ -17,17 +84,20 @@ const InqueryManagement = () => {
                 <div className="check_user_ip_title_div">
                     <p id="change_review_title">Check User & Block User Id</p>
                     <div className="check_user_ip_search_div">
-                        <form id="doctor_edit_page_search_form">
+                        <form id="doctor_edit_page_search_form" onSubmit={(e) => onSubmit(e)}>
                             <input
                                 type="text"
                                 id="doctor_edit_page_search_input"
                                 placeholder="원하는 게시글을 검색하세요."
+                                value={id}
+                                onChange={(e) => setId(e.target.value)}
                             />
                         </form>
                         <img
                             src="/search.png"
                             alt="search"
                             id="doctor_edit_page_search_button"
+                            onClick={onClick}
                         />
                     </div>
                 </div>
@@ -46,28 +116,30 @@ const InqueryManagement = () => {
                             </div>
                         </div>
                         <div className="inquery_items_div">
-                            {item.map((item, index) => {
+                            {inquiryList.length !== 0 ? inquiryList.map((item, index) => {
                                 return (
-                                    <div className="inquery_item_div">
+                                    index < 10 ?
+                                    <div className="inquery_item_div" key={item.id}>
                                         <div className="check_user_index_left_div">
-                                            <p id="inquery_date">2023.08.11</p>
+                                            <p id="inquery_date">{item.createdAt}</p>
                                             <p id="inquery_id_data">
-                                                kimchulsoo@gmail.com
+                                                {item.userId}
                                             </p>
                                         </div>
                                         <div className="check_user_index_right_div">
-                                            <p id="inquery_answer_data">
-                                                미답변
+                                            <p id={item.answer === null ? "inquery_answer_data" : "inquery_answer_data_click"}>
+                                                {item.answer === null ? "미답변" : "답변완료"}
                                             </p>
                                             <div className="inquery_action_div">
                                                 <p id="inquery_action_data">
-                                                    답변
+                                                    answer
                                                 </p>
                                             </div>
                                         </div>
                                     </div>
+                                    : null
                                 );
-                            })}
+                            }) : null}
                         </div>
                     </div>
                     <div className="check_user_left_div">
@@ -82,28 +154,29 @@ const InqueryManagement = () => {
                             </div>
                         </div>
                         <div className="inquery_items_div">
-                            {item.map((item, index) => {
+                            {inquiryList.length !== 0 ? inquiryList.map((item, index) => {
                                 return (
+                                    10 <= index && index < 20 ?
                                     <div className="inquery_item_div">
                                         <div className="check_user_index_left_div">
-                                            <p id="inquery_date">2023.08.11</p>
+                                            <p id="inquery_date">{item.createdAt}</p>
                                             <p id="inquery_id_data">
-                                                kimchulsoo@gmail.com
+                                                {item.userId}
                                             </p>
                                         </div>
                                         <div className="check_user_index_right_div">
-                                            <p id="inquery_answer_data">
-                                                미답변
+                                            <p id={item.answer === null ? "inquery_answer_data" : "inquery_answer_data_click"}>
+                                                {item.answer === null ? "미답변" : "답변완료"}
                                             </p>
                                             <div className="inquery_action_div">
                                                 <p id="inquery_action_data">
-                                                    답변
+                                                    answer
                                                 </p>
                                             </div>
                                         </div>
-                                    </div>
+                                    </div> : null
                                 );
-                            })}
+                            }): null}
                         </div>
                     </div>
                 </div>
