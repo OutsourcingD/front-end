bc4b6b9fbc0980a6ff0d7b82d52db22be5539add
Qkdrmf <djs110615@mju.ac.kr>
2023-10-10 02:44:50 +0900

    Refactor: error 처리

--- src/components/ContentInput.tsx
+++ src/components/ContentInput.tsx
@@ -10,78 +10,82 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Wysiwyg = () => {
-  const editorRef = React.useRef<ToastEditor>(null);
-  const [data, setData] = React.useState<string | null>(null);
-  const navigate = useNavigate();
+    const editorRef = React.useRef<ToastEditor>(null);
+    const [data, setData] = React.useState<string | null>(null);
+    const navigate = useNavigate();
 
-  const onUploadImage = async (blob: Blob, callback: HookCallback) => {
-    const formData = new FormData();
-    formData.append("file", blob);
+    const onUploadImage = async (blob: Blob, callback: HookCallback) => {
+        const formData = new FormData();
+        formData.append("file", blob);
 
-    await axios({
-      method: "post",
-      url: `${process.env.REACT_APP_SERVER_URL}/image`,
-      data: formData,
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
-      },
-    }).then((res) => {
-      callback(res.data, "");
-    }).catch((err) => {
-      if(err.response.status === 403 || err.response.status === 401) {
-          alert("This is not admin ID.");
-          navigate("/login");
-      }
-      else {
-          alert(`Contact to developer. ${err.response.status}`);
-          navigate("/");   
-      }
-  });
-  };
+        await axios({
+            method: "post",
+            url: `${process.env.REACT_APP_SERVER_URL}/image`,
+            data: formData,
+            headers: {
+                "Content-Type": "multipart/form-data",
+                Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
+            },
+        })
+            .then((res) => {
+                callback(res.data, "");
+            })
+            .catch((err) => {
+                if (
+                    err.response.status === 403 ||
+                    err.response.status === 401
+                ) {
+                    alert("This is not admin ID.");
+                    navigate("/login");
+                } else {
+                    alert(`Contact to developer. ${err.response.status}`);
+                    navigate("/");
+                }
+            });
+    };
 
-  const onChange = () => {
-    const htmlValue = editorRef.current?.getInstance().getHTML() || null;
-    setData(htmlValue);
-  };
+    const onChange = () => {
+        const htmlValue = editorRef.current?.getInstance().getHTML() || null;
+        setData(htmlValue);
+    };
 
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-      }}
-    >
-      <div style={{ width: "100%", height: "100%" }}>
-        <Box sx={{ m: 2 }}>
-          <ToastEditor
-            ref={editorRef}
-            height="500px"
-            previewStyle="vertical" // tab or vertical
-            initialEditType="wysiwyg" // or markdown
-            hideModeSwitch={true} // 하단 숨기기
-            toolbarItems={[
-              ["heading", "bold", "italic", "strike"],
-              ["hr", "quote"],
-              ["ul", "ol", "task", "indent", "outdent"],
-              ["table", "image", "link"],
-              ["code", "codeblock"],
-            ]}
-            usageStatistics={false} // 통계 수집 거부
-            initialValue=" "
-            plugins={[colorSyntax]}
-            onChange={onChange}
-            hooks={{
-              addImageBlobHook: onUploadImage,
+    return (
+        <div
+            style={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
             }}
-          />
-        </Box>
-      </div>    
-    </div>
-  );
+        >
+            <div style={{ width: "100%", height: "100%" }}>
+                <Box sx={{ m: 2 }}>
+                    <ToastEditor
+                        ref={editorRef}
+                        height="500px"
+                        previewStyle="vertical" // tab or vertical
+                        initialEditType="wysiwyg" // or markdown
+                        hideModeSwitch={true} // 하단 숨기기
+                        toolbarItems={[
+                            ["heading", "bold", "italic", "strike"],
+                            ["hr", "quote"],
+                            ["ul", "ol", "task", "indent", "outdent"],
+                            ["table", "image", "link"],
+                            ["code", "codeblock"],
+                        ]}
+                        usageStatistics={false} // 통계 수집 거부
+                        initialValue=" "
+                        plugins={[colorSyntax]}
+                        onChange={onChange}
+                        hooks={{
+                            addImageBlobHook: onUploadImage,
+                        }}
+                    />
+                </Box>
+            </div>
+        </div>
+    );
 };
 
 export default Wysiwyg;
