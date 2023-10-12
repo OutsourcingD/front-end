a2384d08dabcccc93da5212b1ff17c1095daa6d2
Qkdrmf <djs110615@mju.ac.kr>
2023-10-12 18:45:09 +0900

    Refactor: find id page css % => px

--- src/home/IdInfoPage.tsx
+++ src/home/IdInfoPage.tsx
@@ -5,21 +5,21 @@ function IdInfoPage() {
   return (
     <div className="id_result_div">
         <div className="id_result_text_div">
-            <p style={{fontSize: "15px", fontWeight: "500", color: "#444444"}}>The ID matches the information you entered.</p>
-            <div style={{display: "flex", flexDirection: "row", marginTop: "7%"}}>
-                <p style={{fontSize: "15px", fontWeight: "500", color: "#444444", alignItems: "center", display: "flex"}}>ID :</p>
-                <p style={{fontSize: "18px", fontWeight: "700", color: "#444444"}}>Kimchulsoo@gmail.com</p>
+            <p id="id_infomanage_title">The ID matches the information you entered.</p>
+            <div className="id_infomanage_div">
+                <p id="id_infomanage_id">ID :</p>
+                <p id="id_infomanage_id_data">Kimchulsoo@gmail.com</p>
             </div>
-            <div style={{display: "flex", flexDirection: "row", marginTop: "2%"}}>
+            <div className="id_infomanage_date_div">
                 <p id="join_date">Join date : </p>
                 <p id="join_date">2018. 10. 33</p>
             </div>
-            <div className="id_result_button_div" style={{display: "flex", flexDirection: "row", marginTop: "15%"}}>
+            <div className="id_result_button_div">
                 <div className="id_result_ok_button">
-                    <p style={{fontSize: "15px", fontWeight: "700", color: "white"}}>OK</p>
+                    <p id="id_result_ok_button_text">OK</p>
                 </div>
                 <div className="id_result_pwd_button">
-                    <p style={{fontSize: "15px", fontWeight: "700", color: "#888888"}}>Find PWD</p>
+                    <p id="id_result_pwd_button_text">Find PWD</p>
                 </div>
             </div>
         </div>
