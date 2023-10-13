bb93654ff28c5d100b8996108aa6f505c0a403cf
QKdrmf <djs110615@mju.ac.kr>
2023-10-13 19:23:12 +0900

    Refactor: footer language

--- src/bottom/Footer.tsx
+++ src/bottom/Footer.tsx
@@ -8,22 +8,22 @@ function Footer() {
   return (
     <div className="footer_body_div">
       <div className="footer_inquery_info_div">
-        <p id="inquiry_item" onClick={() => navigate("/inquery")}>문의하기</p>
+        <p id="inquiry_item" onClick={() => navigate("/inquiry")}>inquiry</p>
         <p id="inquiry_item">|</p>
-        <p id="inquiry_item">개인정보처리방침</p>
+        <p id="inquiry_item">privacy policy</p>
         <p id="inquiry_item">|</p>
-        <p id="inquiry_item">이용약관</p>
+        <p id="inquiry_item">terms</p>
       </div>
       <div className="footer_info_div">
-        <p id="inquiry">상호명: K-Surgery-forum</p>
-        <p id="inquiry">사업자 등록번호: 178-88-02857</p>
-        <p id="inquiry">사업장 소재지: 서울시동작구 사당로22나길 30, 1층</p>
+        <p id="inquiry">business name: K-Surgery-forum</p>
+        <p id="inquiry">Company registration number: 178-88-02857</p>
+        <p id="inquiry">Company location: 30, Sadang-ro 22na-gil, Dongjak-gu, Seoul, Republic of Korea</p>
       </div>
 
       <div className="footer_top_div">
-        <p id="inquiry">대표자: 박지영</p>
+        <p id="inquiry">Representative: Ji Young Park</p>
         <p id="inquiry">Email: surgery-forum23@naver.com</p>
-        <p id="inquiry">전화번호: 0000-0000</p>
+        <p id="inquiry">tel: 0000-0000</p>
       </div>
     </div>
   );
