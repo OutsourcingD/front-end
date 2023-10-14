56f6a081fa58787ce19d2bd43a26699df3fc85e9
Qkdrmf <djs110615@mju.ac.kr>
2023-10-14 23:05:50 +0900

    Feat: 아이디/비번 찾기

--- src/home/PwdSettingPage.tsx
+++ src/home/PwdSettingPage.tsx
@@ -1,31 +1,74 @@
 import React from "react";
-import "./PwdSettingPage.css"
+import "./PwdSettingPage.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-function PwdSettingPage() {
-  return (
-    <div className="pwd_setting_div">
-        <div className="pwd_setting_info">
-            <p id="pwd_setting_title">Please reset your password.</p>
-        </div>
-        <div className="pwd_setting_input_div">
-            <p id="pwd_setting_pwd_input_div">password</p>
-            <form id="setting_pwd_input_form">
-                <input type="password" placeholder="Please enter a password of 8 to 16 characters." id="pwd_setting_pwd_input" />
-            </form>
-        </div>
-        <div className="pwd_setting_input_div">
-            <p id="pwd_setting_pwd_input_div">password</p>
-            <form id="setting_pwd_input_form">
-                <input type="password" placeholder="Enter password again." id="pwd_setting_pwd_input" />
-            </form>
-        </div>
-        <div className="pwd_setting_check_button_div">
-            <div className="pwd_setting_check_button">
-                <p id="pwd_setting_check_button">OK</p>
+interface PwdSettingPageProps {
+    userId: string;
+}
+
+function PwdSettingPage(props: PwdSettingPageProps) {
+    const [pwd, setPwd] = React.useState<string>("");
+    const [pwdCheck, setPwdCheck] = React.useState<string>("");
+    const navigate = useNavigate();
+
+    const onClick = () => {
+        if (pwd === pwdCheck) {
+            axios({
+                method: "post",
+                url: `${process.env.REACT_APP_SERVER_URL}/email/password/change`,
+                data: {
+                    newPassword: pwd,
+                    userId: props.userId,
+                },
+            }).then((res) => {
+                alert("Password reset complete.");
+                navigate("/login");
+            }).catch((err) => {
+                alert(`Contact to developer. ${err.response.status}`);
+            })
+        }
+        else {
+            alert("Please check password.");
+        }
+    }
+
+    return (
+        <div className="pwd_setting_div">
+            <div className="pwd_setting_info">
+                <p id="pwd_setting_title">Please reset your password.</p>
+            </div>
+            <div className="pwd_setting_input_div">
+                <p id="pwd_setting_pwd_input_div">password</p>
+                <form id="setting_pwd_input_form">
+                    <input
+                        type="password"
+                        placeholder="Please enter a password of 8 to 16 characters."
+                        id="pwd_setting_pwd_input"
+                        value={pwd}
+                        onChange={(e) => setPwd(e.target.value)}
+                    />
+                </form>
+            </div>
+            <div className="pwd_setting_input_div">
+                <p id="pwd_setting_pwd_input_div">password</p>
+                <form id="setting_pwd_input_form">
+                    <input
+                        type="password"
+                        placeholder="Enter password again."
+                        id="pwd_setting_pwd_input"
+                        value={pwdCheck}
+                        onChange={(e) => setPwdCheck(e.target.value)}
+                    />
+                </form>
+            </div>
+            <div className="pwd_setting_check_button_div">
+                <div className="pwd_setting_check_button" onClick={onClick}>
+                    <p id="pwd_setting_check_button">OK</p>
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default PwdSettingPage;
