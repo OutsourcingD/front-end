450a3d505bb398e55b8660e0679ef4ff0959da77
Qkdrmf <djs110615@mju.ac.kr>
2023-10-26 17:35:10 +0900

    Refactor: check user info에서 password 삭제

--- src/components/CheckUser.tsx
+++ src/components/CheckUser.tsx
@@ -340,22 +340,18 @@ const CheckUser = () => {
                                     <div className="user_detail_right_div">
                                         <div className="user_detail_top_left_password_div">
                                             <div className="user_detail_email_div">
-                                                <p id="user_detail_email">password</p>
-                                                <p id="user_detail_email_data">{detail.password ? detail.password : "error"}</p>
-                                            </div>
-                                            <div className="user_detail_email_div">
                                                 <p id="user_detail_email">phone</p>
                                                 <p id="user_detail_email_data">{detail.phoneNumber === null ? "N/A" : detail.phoneNumber}</p>
+                                            </div>
+                                            <div className="user_detail_email_div">
+                                                <p id="user_detail_email">gender</p>
+                                                <p id="user_detail_email_data">{detail.gender === undefined ? "N/A" : detail.gender ===  "true" ? "male" : "female"}</p>
                                             </div>
                                         </div>
                                         <div className="user_detail_top_left_info_div">
                                             <div className="user_detail_email_div">
                                                 <p id="user_detail_email">nickname</p>
                                                 <p id="user_detail_email_data">{detail.nickname}</p>
-                                            </div>
-                                            <div className="user_detail_email_div">
-                                                <p id="user_detail_email">gender</p>
-                                                <p id="user_detail_email_data">{detail.gender === undefined ? "N/A" : detail.gender ===  "true" ? "male" : "female"}</p>
                                             </div>
                                         </div>
                                     </div>
